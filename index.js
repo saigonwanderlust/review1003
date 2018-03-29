12b95aa4a46f2b0331e4ef197463fd7997ed0019
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-29 17:40:27 +0700

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const reload = require('reload');
+const { Singer, singers } = require('./Singer');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', { singers });
+});
+
+app.get('/add', (req, res) => res.render('add'));
+
+app.listen(3000, () => console.log('Server started!'));
+reload(app);
