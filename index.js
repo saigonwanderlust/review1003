786d25c3b54c58df3245331fee87c45dcbf90e6d
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-29 18:22:01 +0700

    req.body

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const reload = require('reload');
 const { Singer, singers } = require('./Singer');
 
+const parser = require('body-parser').urlencoded({ extended: false });
+
 const app = express();
+
+app.use(parser);
 
 app.set('view engine', 'ejs');
 
@@ -10,7 +14,20 @@ app.get('/', (req, res) => {
     res.render('index', { singers });
 });
 
+app.get('/remove/:id', (req, res) => {
+    const { id } = req.params;
+    const index = singers.findIndex(singer => +singer.id === id);
+    if (index === -1) return res.send('Khong tim thay');
+    singers.splice(index, 1);
+    res.redirect('/');
+});
+
 app.get('/add', (req, res) => res.render('add'));
+
+app.post('/add', (req, res) => {
+    console.log(req.body);
+    res.send('Da nhan duoc.');
+});
 
 app.listen(3000, () => console.log('Server started!'));
 reload(app);
